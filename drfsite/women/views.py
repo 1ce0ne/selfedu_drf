ed917ded23cd5e85e4d547ed45d94ad91ba0db0a
Aleksandr Chernakov <alexandertschernakow@gmail.com>
2023-03-20 13:45:18 +0300

    Урок 3: Базовый класс APIView

--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -2,8 +2,40 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Women
 from .serializers import WomenSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.forms import model_to_dict
+
 
 # Урок 2: Пример простого представления
-class WomenAPIView(generics.ListAPIView):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer
+# class WomenAPIView(generics.ListAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
+
+# Урок 3: Базовый класс APIView 
+class WomenAPIView(APIView):
+    # Обработка GET-запросов:
+    def get(self, request):
+        lst = Women.objects.all().values()
+        return Response({'posts': list(lst)})
+    
+    # Обработка POST-запросов:
+    def post(self, request):
+        post_new = Women.objects.create(
+            title=request.data['title'],
+            content=request.data['content'],
+            cat_id=request.data['cat_id'],
+        ) 
+        return Response({'post': model_to_dict(post_new)})
+
+#     def get(self, request):
+#         lst = Women.objects.all().values()
+#         return Response({'posts': list(lst)})
+
+#     def post(self, request):
+#         post_new = Women.objects.create(
+#             title=request.data['title'],
+#             content=request.data['content'],
+#             cat_id=request.data['cat_id'],
+#         )
+#         return Response({'post': model_to_dict(post_new)})
