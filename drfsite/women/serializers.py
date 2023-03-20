4fc8215f87e8329fea2d808b9bf1f0360e47b51f
Aleksandr Chernakov <alexandertschernakow@gmail.com>
2023-03-20 14:22:08 +0300

    Урок 4: Введение в сериализацию

--- drfsite/women/serializers.py
+++ drfsite/women/serializers.py
@@ -1,8 +1,50 @@
 from rest_framework import serializers
 from .models import Women
+from rest_framework.renderers import JSONRenderer
+import io
+from rest_framework.parsers import JSONParser
 
 # Урок 2: Пример простого сериализатора
-class WomenSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Women
-        fields = ('title', 'cat_id') 
+# class WomenSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Women
+#         fields = ('title', 'cat_id') 
+
+
+# Урок 3: Введение в сериализацию
+# class WomenModel:
+#     def __init__(self, title, content):
+#         self.title = title
+#         self.content = content
+
+# class WomenSerializer(serializers.Serializer):
+#     title = serializers.CharField(max_length=255)
+#     content = serializers.CharField()
+
+# def encode():
+#     """
+#     Инструкция:
+#         1. python3 manage.py shell
+#         2. from women.serializers import encode
+#         3. encode()
+#     """
+#     model = WomenModel('Genevieve Richter', 'Content: TheVzryv')
+#     model_sr = WomenSerializer(model)
+#     print(model_sr.data, type(model_sr.data), sep='\n')
+#     json = JSONRenderer().render(model_sr.data)
+#     print(json)
+
+# def decode():
+#     stream = io.BytesIO(b'{"title":"Genevieve Richter","content":"Content: TheVzryv"}')
+#     data = JSONParser().parse(stream)
+#     serializers = WomenSerializer(data=data)
+#     serializers.is_valid()
+#     print(serializers.validated_data)
+
+class WomenSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+    time_create = serializers.DateTimeField(read_only=True)
+    time_update = serializers.DateTimeField(read_only=True)
+    is_published = serializers.BooleanField(default=True)
+    cat_id = serializers.IntegerField()
--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -12,21 +12,22 @@ from django.forms import model_to_dict
 #     queryset = Women.objects.all()
 #     serializer_class = WomenSerializer
 
+
 # Урок 3: Базовый класс APIView 
-class WomenAPIView(APIView):
-    # Обработка GET-запросов:
-    def get(self, request):
-        lst = Women.objects.all().values()
-        return Response({'posts': list(lst)})
+# class WomenAPIView(APIView):
+#     # Обработка GET-запросов:
+#     def get(self, request):
+#         lst = Women.objects.all().values()
+#         return Response({'posts': list(lst)})
     
-    # Обработка POST-запросов:
-    def post(self, request):
-        post_new = Women.objects.create(
-            title=request.data['title'],
-            content=request.data['content'],
-            cat_id=request.data['cat_id'],
-        ) 
-        return Response({'post': model_to_dict(post_new)})
+#     # Обработка POST-запросов:
+#     def post(self, request):
+#         post_new = Women.objects.create(
+#             title=request.data['title'],
+#             content=request.data['content'],
+#             cat_id=request.data['cat_id'],
+#         ) 
+#         return Response({'post': model_to_dict(post_new)})
 
 #     def get(self, request):
 #         lst = Women.objects.all().values()
@@ -39,3 +40,24 @@ class WomenAPIView(APIView):
 #             cat_id=request.data['cat_id'],
 #         )
 #         return Response({'post': model_to_dict(post_new)})
+
+
+# Урок 4: Введение в сериализацию
+class WomenAPIView(APIView):
+    # Обработка GET-запросов:
+    def get(self, request):
+        w = Women.objects.all()
+        return Response({'posts': WomenSerializer(w, many=True).data})
+    
+    # Обработка POST-запросов:
+    def post(self, request):
+        # Проверяем корректность данных:
+        serializers = WomenSerializer(data=request.data)
+        serializers.is_valid(raise_exception=True)
+
+        post_new = Women.objects.create(
+            title=request.data['title'],
+            content=request.data['content'],
+            cat_id=request.data['cat_id'],
+        ) 
+        return Response({'post': WomenSerializer(post_new).data})
